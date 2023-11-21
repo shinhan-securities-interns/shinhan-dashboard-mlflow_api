39a1e17ee21cd7ed9381ae7e822862d1a1850e87
sangeun <grace0857@naver.com>
2023-11-21 10:09:06 +0900

    [FEAT] update KOSPI & KOSDAQ

--- main.py
+++ main.py
@@ -60,24 +60,21 @@ async def predict_KOSPI() :
     IXIC = fdr.DataReader('IXIC', '2013')
     #print(IXIC)
     IXIC_close = np.asarray( IXIC['Close'])
-    print(IXIC_close)
+    #print(IXIC_close)
 
     IXIC_actual_prices = IXIC_close[-1] - IXIC_close[-2]
-    print(IXIC_actual_prices)
+    #print(IXIC_actual_prices)
 ########################################
 
     # 2차원 배열을 1차원 리스트로 변환
     stock_predict = pred.flatten().tolist()
     #print(stock_predict)
 
-
-    # 캐시된 데이터가 없으면 데이터 생성 및 JSON 직렬화 후 Redis에 저장
-
     # 실제 값과 예측값을 numpy 배열로 변환
     actual_prices = np.asarray(y_test)[50:]
-    predicted_prices = np.asarray(pred).flatten()  # pred가 2차원 배열인 경우 flatten 사용
+    predicted_prices = np.asarray(pred).flatten()  # pred가 2차원 배열인 경우 flatten 사용    
     # 실제 값과 예측값의 다음 날 변화 계산
-    actual_changes = actual_prices[1:] - actual_prices[:-1]
+    #actual_changes = actual_prices[1:] - actual_prices[:-1]
 
 
     # predicted_changes = (predicted_prices[-1]) - predicted_prices[-2]
@@ -109,24 +106,24 @@ async def predict_KOSDAQ() :
     IXIC = fdr.DataReader('IXIC', '2013')
     #print(IXIC)
     IXIC_close = np.asarray(IXIC['Close'])
-    print(IXIC_close)
+    #print(IXIC_close)
 
     IXIC_actual_prices = IXIC_close[-1] - IXIC_close[-2]
-    print(IXIC_actual_prices)
+    #print(IXIC_actual_prices)
 ########################################
 
     # 2차원 배열을 1차원 리스트로 변환
     stock_predict = pred.flatten().tolist()
     #print(stock_predict)
 
-
-    # 캐시된 데이터가 없으면 데이터 생성 및 JSON 직렬화 후 Redis에 저장
-
     # 실제 값과 예측값을 numpy 배열로 변환
     actual_prices = np.asarray(y_test)[50:]
     predicted_prices = np.asarray(pred).flatten()  # pred가 2차원 배열인 경우 flatten 사용
+
+    #print(predicted_prices)
+
     # 실제 값과 예측값의 다음 날 변화 계산
-    actual_changes = actual_prices[1:] - actual_prices[:-1]
+    #actual_changes = actual_prices[1:] - actual_prices[:-1]
     
     #predicted_changes = (predicted_prices[-1]) - predicted_prices[-2]
 
@@ -140,7 +137,7 @@ async def predict_KOSDAQ() :
     most_recent_prediction = predicted_directions
 
     result = most_recent_prediction
-    print(most_recent_prediction)
+    #print(most_recent_prediction)
 
     # 결과를 캐시에 저장
     await app.state.mlflow.setKey(key, result, 60 * 60 * 24)
@@ -148,7 +145,7 @@ async def predict_KOSDAQ() :
 @app.get("/predict/kospi_kosdaq")
 async def get_kospi_kosdaq_prediction():
 
-    await predict_KOSDAQ()
+    await predict_KOSPI()
     await predict_KOSDAQ()
 
     return {
