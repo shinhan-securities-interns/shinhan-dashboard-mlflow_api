8313f3ff27d4f7226872d8874db31015f51f92fb
sangeun <grace0857@naver.com>
2023-11-22 08:32:40 +0900

    [FEAT] mlflow api

--- main.py
+++ main.py
@@ -2,21 +2,23 @@
 import mlflow
 from fastapi import FastAPI
 from schemas import PredictIn, PredictOut
-from data import preprocess_data 
-import numpy as np
 
+from data import preprocess_data 
+
+import numpy as np
+from databases import Database
+
+import FinanceDataReader as fdr
 
 import json
 import os
 import logging
-import database.RedisDriver
-
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-import asyncio
-
+from database.RedisDriver import RedisDriver
 
 app = FastAPI()
+
+DATABASE_URL = "mysql+pymysql://admin:abcd1234!@database-1.coibefbchrij.ap-northeast-2.rds.amazonaws.com:3306/mys2d"
+database = Database(DATABASE_URL)
 
 @app.on_event("startup")
 async def startup_event():
@@ -27,12 +29,13 @@ async def startup_event():
     )
 
     print("startup_event")
+    await database.connect()
+
     try:
-        app.state.mlflow = database.RedisDriver.RedisDriver(f"{REDIS_HOST}:{REDIS_PORT}/12")
+        app.state.mlflow = RedisDriver(f"localhost:6322/12")
 
     except Exception as e:
         print(f"An error occurred during startup: {e}")
-
 
 def get_model_production():
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
@@ -41,6 +44,7 @@ def get_model_production():
     return model_p
 MODEL = get_model_production()
 
+
 ###################################3
 # def get_model():
 #     model = mlflow.keras.load_model(model_uri="./model")
@@ -48,8 +52,8 @@ MODEL = get_model_production()
 # MODEL = get_model()
 
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', '6322')
+# REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+# REDIS_PORT = os.environ.get('REDIS_PORT', '6322')
 
 
 async def predict_KOSPI() :
@@ -57,42 +61,34 @@ async def predict_KOSPI() :
     key = "KOSPI_PREDICTION"
     train_data, test_data, y_test = preprocess_data(stock_code=stock_code, window_size=50, batch_size=32)
     pred = MODEL.predict(test_data)
-    print(pred)
+    #print(pred)
+
+
+###########나스닥 계산##################
+    IXIC = fdr.DataReader('IXIC', '2013')
+    IXIC_close = np.asarray(IXIC['Close'])
+    IXIC_actual_prices = IXIC_close[-1] - IXIC_close[-2]
+########################################
 
     # 2차원 배열을 1차원 리스트로 변환
     stock_predict = pred.flatten().tolist()
-    print(stock_predict)
-
-
-    # 캐시된 데이터가 없으면 데이터 생성 및 JSON 직렬화 후 Redis에 저장
 
     # 실제 값과 예측값을 numpy 배열로 변환
     actual_prices = np.asarray(y_test)[50:]
-    predicted_prices = np.asarray(pred).flatten()  # pred가 2차원 배열인 경우 flatten 사용
-    # 실제 값과 예측값의 다음 날 변화 계산
-    actual_changes = actual_prices[1:] - actual_prices[:-1]
-    predicted_changes = (predicted_prices[-1]) - predicted_prices[-2]
-    # 변화 방향 예측
-    predicted_directions = "up" if predicted_changes > 0 else "down"
-    # predicted_directions 배열의 마지막 요소 추출
-    most_recent_prediction = predicted_directions
+    predicted_prices = np.asarray(pred).flatten()
+    
+    #예측된 값에 나스닥 영향 주기 
+    predicted_changes = (predicted_prices[-1] + (2**-10 * IXIC_actual_prices)) - predicted_prices[-2]
+    print("KOSPI 예측 변화율: ", predicted_changes)
 
-    result = most_recent_prediction
-    print(most_recent_prediction)
+    # 변화 방향 예측
+    predicted_directions = "상승" if predicted_changes > 0 else "하강"
+    
+    result = predicted_directions
+    print(predicted_directions)
 
     # 결과를 캐시에 저장
     await app.state.mlflow.setKey(key, result, 60 * 60 * 24)
-
-    # 스케줄러 설정
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(predict_KOSPI, CronTrigger(day_of_week='mon-sat', hour=8, minute=30))
-
-    # 스케줄러 시작
-    scheduler.start()
-
-    # 이벤트 루프 실행
-    loop = asyncio.get_event_loop()
-    loop.run_forever()
 
 
 async def predict_KOSDAQ() :
@@ -100,51 +96,54 @@ async def predict_KOSDAQ() :
     key = "KOSDAQ_PREDICTION"
     train_data, test_data, y_test = preprocess_data(stock_code=stock_code, window_size=50, batch_size=32)
     pred = MODEL.predict(test_data)
-    print(pred)
+    #print(pred)
+
+###########나스닥 계산##################
+    IXIC = fdr.DataReader('IXIC', '2013')
+    IXIC_close = np.asarray(IXIC['Close'])
+    IXIC_actual_prices = IXIC_close[-1] - IXIC_close[-2]
+########################################
 
     # 2차원 배열을 1차원 리스트로 변환
     stock_predict = pred.flatten().tolist()
-    print(stock_predict)
-
-
-    # 캐시된 데이터가 없으면 데이터 생성 및 JSON 직렬화 후 Redis에 저장
 
     # 실제 값과 예측값을 numpy 배열로 변환
     actual_prices = np.asarray(y_test)[50:]
-    predicted_prices = np.asarray(pred).flatten()  # pred가 2차원 배열인 경우 flatten 사용
-    # 실제 값과 예측값의 다음 날 변화 계산
-    actual_changes = actual_prices[1:] - actual_prices[:-1]
-    predicted_changes = (predicted_prices[-1]) - predicted_prices[-2]
-    # 변화 방향 예측
-    predicted_directions = "up" if predicted_changes > 0 else "down"
-    # predicted_directions 배열의 마지막 요소 추출
-    most_recent_prediction = predicted_directions
+    predicted_prices = np.asarray(pred).flatten() 
 
-    result = most_recent_prediction
-    print(most_recent_prediction)
+    #예측된 값에 나스닥 영향 주기 
+    predicted_changes = (predicted_prices[-1] + (2**-10 * IXIC_actual_prices)) - predicted_prices[-2]
+    print("KOSDAQ 예측 변화: ", predicted_changes)
+
+
+    # 변화 방향 예측
+    predicted_directions = "상승" if predicted_changes > 0 else "하강"
+    # predicted_directions 배열의 마지막 요소 추출
+    # most_recent_prediction = predicted_directions
+
+    result = predicted_directions
+    print(predicted_directions)
 
     # 결과를 캐시에 저장
     await app.state.mlflow.setKey(key, result, 60 * 60 * 24)
 
-    # 스케줄러 설정
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(predict_KOSDAQ, CronTrigger(day_of_week='mon-sat', hour=8, minute=30))
 
-    # 스케줄러 시작
-    scheduler.start()
-
-    # 이벤트 루프 실행
-    loop = asyncio.get_event_loop()
-    loop.run_forever()
-
-@app.get("/predict/kospi_kosdaq")
+@app.post("/predict/kospi_kosdaq")
 async def get_kospi_kosdaq_prediction():
 
-    await predict_KOSDAQ()
+    await predict_KOSPI()
     await predict_KOSDAQ()
 
     return {
         "KOSPI_PREDICTION" : await app.state.mlflow.getKey("KOSPI_PREDICTION"),
         "KOSDAQ_PREDICTION": await app.state.mlflow.getKey("KOSDAQ_PREDICTION"),
     }
+
+@app.post("/insert-prediction/")
+async def insert_prediction():
+    query = "INSERT INTO prediction (kospi, kosdaq) VALUES (:kospi, :kosdaq)"
+    values = {"kospi": f"{await predict_KOSPI()}", "kosdaq": f"{await predict_KOSDAQ()}"}
+
+    await database.execute(query, values)
+    return {"message": "Data inserted successfully"}
 
---
+++ model.py
@@ -0,0 +1,225 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import FinanceDataReader as fdr
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
+
+# DL
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers import Conv1D,LSTM, Dense
+from keras.optimizers import Adam
+
+from tensorflow.keras.losses import Huber
+from tensorflow.keras.callbacks import EarlyStopping
+
+import mlflow
+import mlflow.keras
+
+################################################################
+# TensorFlow Dataset을 활용한 시퀸스 데이터셋 구성
+def windowed_dataset(series, window_size, batch_size, shuffle):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    if shuffle:
+        ds = ds.shuffle(1000)
+    ds = ds.map(lambda w: (w[:-1], w[-1]))
+    return ds.batch(batch_size).prefetch(1)
+
+def create_mlflow_experiment(mlflow_uri, experiment_name):
+    mlflow.set_tracking_uri(mlflow_uri)
+    exp_id = mlflow.create_experiment(experiment_name)
+    return exp_id
+
+
+
+## 1.데이터 전처리 함수 정의
+def preprocess_data(stock_code, window_size=50, batch_size=32):
+    # 데이터 수집 및 전처리를 위한 stock_code, window_size, batch_size 인수 사용
+    STOCK_CODE = stock_code
+    WINDOW_SIZE = window_size
+    BATCH_SIZE = batch_size
+    
+    stock = fdr.DataReader(STOCK_CODE, '2013')
+
+    scaler = MinMaxScaler()
+    scale_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
+    scaled = scaler.fit_transform(stock[scale_cols])
+    df = pd.DataFrame(scaled, columns=scale_cols)
+
+    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns=['Close']), df['Close'], test_size=0.2, random_state=20, shuffle=False)
+
+    train_data = windowed_dataset(y_train, WINDOW_SIZE, BATCH_SIZE, True)
+    test_data = windowed_dataset(y_test, WINDOW_SIZE, BATCH_SIZE, False)
+
+    return train_data, test_data
+
+
+# 2. MLflow 초기화 및 추적 URI 설정
+mlflow_uri = "sqlite:///mlflow.db"
+mlflow.set_tracking_uri(mlflow_uri)
+
+
+# 3. 실험 생성 및 실험 ID 얻기
+experiment_name = "StockPrediction"
+exp_id = create_mlflow_experiment(mlflow_uri, experiment_name)
+
+# 주식 코드
+stock_code = 'KS11'
+
+# 4. 각 모델 훈련 루프 실행
+for i in range(1, 2):
+    # 1번째 모델
+    with mlflow.start_run(experiment_id=exp_id, run_name = 'dl_autolog_1'):
+        mlflow.keras.autolog()
+
+        # Preprocess the data using the provided stock code
+        WINDOW_SIZE = 50
+        BATCH_SIZE = 32
+        train_data, test_data = preprocess_data(stock_code, WINDOW_SIZE, BATCH_SIZE)
+
+        # Model design and compilation
+        model = Sequential([
+            Conv1D(filters=64, kernel_size=10, padding="same", activation="relu", input_shape=[WINDOW_SIZE, 1]),
+            LSTM(16, activation='tanh'),
+            Dense(16, activation="relu"),
+            Dense(1)
+        ])
+        model.compile(loss=Huber(), optimizer=Adam(0.0005))
+
+        # Early stopping
+        early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+        # Training
+        history = model.fit(train_data, validation_data=test_data, epochs=50, callbacks=[early_stopping]).history
+
+        # Log the model
+        mlflow.keras.log_model(model, "model", registered_model_name= "stock_prediction")
+
+        # Disable autolog
+        mlflow.keras.autolog(disable=True)
+
+
+    ######################################
+    # 2 번째 모델  
+    with mlflow.start_run(experiment_id=exp_id, run_name='dl_autolog_2'):
+        
+        mlflow.keras.autolog()
+
+        # Preprocess the data using the provided stock code
+        WINDOW_SIZE = 50
+        BATCH_SIZE = 32
+        train_data, test_data = preprocess_data(stock_code, WINDOW_SIZE, BATCH_SIZE)
+
+    #모델 설계 및 컴파일
+        model2 = Sequential([
+            # 1차원 feature map 생성
+            Conv1D(filters=64, kernel_size=5,
+            padding="same",
+            activation="relu",
+            input_shape=[WINDOW_SIZE, 1]),
+            # LSTM
+            LSTM(16, activation='tanh'),
+            Dense(16, activation="relu"),
+            Dense(1) ])
+        # Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+        loss = Huber()
+        optimizer = Adam(0.0005)
+        model2.compile(loss=Huber(), optimizer= optimizer)
+        # earlystopping은 10번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+        earlystopping = EarlyStopping(monitor='val_loss', patience=10)
+    #학습
+        history = model2.fit(train_data,
+                        validation_data=(test_data),
+                        epochs=50,
+                        callbacks=[earlystopping]).history
+    #모델 등록 (best 모델 기록하기)
+        mlflow.keras.log_model(model2, "model2",  registered_model_name="stock_prediction")
+    #autolog 종료
+        mlflow.keras.autolog(disable = True)
+
+
+    ######################################
+    # 3 번째 모델  
+    with mlflow.start_run(experiment_id=exp_id, run_name='dl_autolog_3'):
+        
+        mlflow.keras.autolog()
+
+        # Preprocess the data using the provided stock code
+        WINDOW_SIZE = 50
+        BATCH_SIZE = 32
+        train_data, test_data = preprocess_data(stock_code, WINDOW_SIZE, BATCH_SIZE)
+
+    #모델 설계 및 컴파일
+        model3 = Sequential([
+            # 1차원 feature map 생성
+            Conv1D(filters=32, kernel_size=3,
+            padding="same",
+            activation="relu",
+            input_shape=[WINDOW_SIZE, 1]),
+            # LSTM
+            LSTM(16, activation='tanh'),
+            Dense(16, activation="relu"),
+            Dense(1) ])
+        # Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+        loss = Huber()
+        optimizer = Adam(0.0005)
+        model3.compile(loss=Huber(), optimizer= optimizer)
+        # earlystopping은 10번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+        earlystopping = EarlyStopping(monitor='val_loss', patience=10)
+    #학습
+        history = model3.fit(train_data,
+                        validation_data=(test_data),
+                        epochs=50,
+                        callbacks=[earlystopping]).history
+    #모델 등록 (best 모델 기록하기)
+        mlflow.keras.log_model(model3, "model3",  registered_model_name="stock_prediction")
+    #autolog 종료
+        mlflow.keras.autolog(disable = True)
+
+
+    ######################################
+    # 4 번째 실행   
+    with mlflow.start_run(experiment_id=exp_id, run_name='dl_autolog_4'):
+        
+        mlflow.keras.autolog()
+
+        # Preprocess the data using the provided stock code
+        WINDOW_SIZE = 50
+        BATCH_SIZE = 32
+        train_data, test_data = preprocess_data(stock_code, WINDOW_SIZE, BATCH_SIZE)
+
+    #모델 설계 및 컴파일
+        model4 = Sequential([
+            # 1차원 feature map 생성
+            Conv1D(filters=64, kernel_size=10,
+            padding="same",
+            activation="relu",
+            input_shape=[WINDOW_SIZE, 1]),
+            # LSTM
+            LSTM(16, activation='tanh'),
+            Dense(32, activation="relu"),
+            Dense(16, activation="relu"),
+            Dense(8, activation="relu"),
+            Dense(1) ])
+        # Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+        loss = Huber()
+        optimizer = Adam(0.0005)
+        model4.compile(loss=Huber(), optimizer= optimizer)
+        # earlystopping은 10번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+        earlystopping = EarlyStopping(monitor='val_loss', patience=10)
+    #학습
+        history = model4.fit(train_data,
+                        validation_data=(test_data),
+                        epochs=50,
+                        callbacks=[earlystopping]).history
+    #모델 등록 (best 모델 기록하기)
+        mlflow.keras.log_model(model4, "model4",  registered_model_name="stock_prediction")
+    #autolog 종료
+        mlflow.keras.autolog(disable = True)
